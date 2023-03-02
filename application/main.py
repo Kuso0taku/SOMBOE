b78bfc23078664a861260e174f6a276e9965ab79
kuso0taku <denustuzan@gmail.com>
2023-03-02 10:25:21 +0700

    app init

---
+++ application/main.py
@@ -0,0 +1,18 @@
+from deepface import DeepFace
+import json
+import cv2
+import tkinter
+from PIL import Image, ImageTk
+
+
+class App():
+
+    def __init__(self):
+        pass
+
+    def main(self):
+        pass
+
+
+if __name__ == '__main__':
+    App().main()
